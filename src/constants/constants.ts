8fe7a0ee728a08719e14c7986a71e2db6b059051
Srenky <srenky@email.com>
2022-02-13 16:48:15 +0100

    changed size of board

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,3 @@
-export const MAX_HEIGHT = 10;
-export const MAX_WIDTH = 20;
-export const N_OF_MINES = 10;
+export const MAX_HEIGHT = 16;
+export const MAX_WIDTH = 30;
+export const N_OF_MINES = 99;
